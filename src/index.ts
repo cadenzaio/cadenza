8f67a1456d3496b843765b99f03454c94e4a33ce
emilforsvall <emil@canaray.com>
2025-07-30 17:15:33 +0100

    feat: More exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Cadenza from "./Cadenza";
+import GraphRun from "./engine/GraphRun";
 import GraphContext from "./graph/context/GraphContext";
 import DebounceTask from "./graph/definition/DebounceTask";
 import EphemeralTask from "./graph/definition/EphemeralTask";
@@ -11,7 +12,7 @@ import SignalMetaTask from "./graph/definition/meta/SignalMetaTask";
 import ThrottledMetaTask from "./graph/definition/meta/ThrottledMetaTask";
 import UniqueMetaTask from "./graph/definition/meta/UniqueMetaTask";
 import SignalTask from "./graph/definition/SignalTask";
-import Task from "./graph/definition/Task";
+import Task, { TaskResult } from "./graph/definition/Task";
 import ThrottledTask from "./graph/definition/ThrottledTask";
 import UniqueTask from "./graph/definition/UniqueTask";
 import SignalEmitter from "./interfaces/SignalEmitter";
@@ -39,5 +40,7 @@ export {
   SignalParticipant,
   GraphContext,
   GraphRegistry,
+  GraphRun,
+  TaskResult,
   AnyObject,
 };
