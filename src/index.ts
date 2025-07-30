dce85f651aa898a5879af47b19ba19088b3bc2d3
emilforsvall <emil@canaray.com>
2025-07-30 17:06:17 +0100

    feat: Added exports of all types of tasks and routines in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 import Cadenza from "./Cadenza";
+import DebounceTask from "./graph/definition/DebounceTask";
+import EphemeralTask from "./graph/definition/EphemeralTask";
+import GraphRoutine from "./graph/definition/GraphRoutine";
+import DebouncedMetaTask from "./graph/definition/meta/DebouncedMetaTask";
+import EphemeralMetaTask from "./graph/definition/meta/EphemeralMetaTask";
+import MetaRoutine from "./graph/definition/meta/MetaRoutine";
+import MetaTask from "./graph/definition/meta/MetaTask";
+import SignalMetaTask from "./graph/definition/meta/SignalMetaTask";
+import ThrottledMetaTask from "./graph/definition/meta/ThrottledMetaTask";
+import UniqueMetaTask from "./graph/definition/meta/UniqueMetaTask";
+import SignalTask from "./graph/definition/SignalTask";
+import Task from "./graph/definition/Task";
+import ThrottledTask from "./graph/definition/ThrottledTask";
+import UniqueTask from "./graph/definition/UniqueTask";
+import SignalEmitter from "./interfaces/SignalEmitter";
+import SignalParticipant from "./interfaces/SignalParticipant";
+import { AnyObject } from "./types/global";
 
 export default Cadenza;
+export {
+  Task,
+  MetaTask,
+  GraphRoutine,
+  MetaRoutine,
+  UniqueTask,
+  UniqueMetaTask,
+  ThrottledTask,
+  ThrottledMetaTask,
+  DebounceTask,
+  DebouncedMetaTask,
+  EphemeralTask,
+  EphemeralMetaTask,
+  SignalTask,
+  SignalMetaTask,
+  SignalEmitter,
+  SignalParticipant,
+  AnyObject,
+};
