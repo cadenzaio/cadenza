6eba1fd3d5d9e240db4ca8cc89eb4eccfbadd31f
emilforsvall <emil@canaray.com>
2025-12-11 12:50:18 +0000

    fix: cleanup

--- src/engine/SignalBroker.ts
+++ src/engine/SignalBroker.ts
@@ -492,18 +492,6 @@ export default class SignalBroker {
     }
   }
 
-  /**
-   * Lists all observed signals.
-   * @returns Array of signals.
-   */
-  listObservedSignals(): string[] {
-    return Array.from(this.signalObservers.keys());
-  }
-
-  listEmittedSignals(): string[] {
-    return Array.from(this.emittedSignalsRegistry);
-  }
-
   reset() {
     this.signalObservers.clear();
     this.emittedSignalsRegistry.clear();
