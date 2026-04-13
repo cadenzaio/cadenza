9b2cd65f31a4b92cb68067f0e37362bd92699163
emilforsvall <emil@canaray.com>
2026-04-13 12:00:12 +0100

    build: add cli bundle target

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,33 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: {
-    entry: "src/index.ts",
-    compilerOptions: {
-      module: "NodeNext",
-      moduleResolution: "NodeNext",
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    external: ["typescript"],
+    dts: {
+      entry: "src/index.ts",
+      compilerOptions: {
+        module: "NodeNext",
+        moduleResolution: "NodeNext",
+      },
+    },
+    sourcemap: true,
+    clean: true,
+    outDir: "dist",
+    tsconfig: "./tsconfig.json",
+  },
+  {
+    entry: ["src/cli.ts"],
+    format: ["cjs"],
+    external: ["typescript"],
+    dts: false,
+    sourcemap: true,
+    clean: false,
+    outDir: "dist",
+    tsconfig: "./tsconfig.json",
+    banner: {
+      js: "#!/usr/bin/env node",
     },
   },
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  tsconfig: "./tsconfig.json", // Explicitly use tsconfig.json
-});
+]);
